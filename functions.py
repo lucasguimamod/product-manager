58d8437890a382c0de958bd132acdf77333b0dd8
Lucas Guimarães Modesto <lucasguimamod00@gmail.com>
2026-09-14 18:54:50 -0300

    Add product listing

--- functions.py
+++ functions.py
@@ -8,3 +8,19 @@ def add_product(products):
     product['amount'] = int(input('Digite a quantidade do produto: '))
     product['category'] = str(input('Digite a categoria deste produto: '))
     products.append(product)
+
+def show_products(products):
+    print('=' * 30)
+    print('PRODUTOS CADASTRADOS'.center(30))
+    print('=' * 30)
+    if len(products) == 0:
+        print('Nenhum produto cadastrado no sistema!')
+        return
+    else:
+        for product in products:
+            print(f'ID: {product['id']}\n'
+                  f'Nome: {product['name']}\n'
+                  f'Preço: {product['price']}\n'
+                  f'Quantidade: {product['amount']}\n'
+                  f'Categoria: {product['category']}\n'
+                  f'{'-' * 30}')
